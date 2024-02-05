28a439cd5d116876c5d7ad77c5b5d1d99c2735aa
Mardjona <Mardjona26@gmail.com>
2024-02-06 00:33:24 +0300

    0

---
+++ Registration/MainWindow.axaml.cs
@@ -0,0 +1,21 @@
+using System.Net.Mime;
+using Avalonia.Controls;
+
+namespace Registration;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+     // public void Flag ()
+    // {
+    //    if (ComboBoxFlags.SelectedIndex == 0)
+     //   {
+     //        MediaTypeNames.Text = "+7";
+     //   }
+      
+   //  }
+}
